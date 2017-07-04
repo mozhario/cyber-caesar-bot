af087d0133b4b91255bb001fd94c9db6992470b2
mozhavro <mozhario@gmail.com>
2017-07-05 01:03:32 +0300

    Moved to flask/webhook

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import os
 import random
+from flask import Flask, request
 
 import config
 import telebot
 
 
 bot = telebot.TeleBot(config.TOKEN)
+
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -28,5 +32,21 @@ def repeat_all_messages(message):
         bot.send_message(message.chat.id, 'Привет, Санёчек ;)')
 
 
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://cyber-caesar-bot.herokuapp.com/bot")
+    return "!", 200
+
+
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+     # bot.polling(none_stop=True)
+
+     server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+     server = Flask(__name__)
