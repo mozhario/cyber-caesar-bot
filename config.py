c38674f2c1733c00559f39ffd9cdd440e974f125
mozhavro <mozhario@gmail.com>
2017-07-05 01:36:01 +0300

    Added config

---
+++ config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+def get_env_variable(var_name):
+    try:
+        return os.getenv(var_name)
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise KeyError(error_msg)
+
+
+TOKEN = get_env_variable("CAESAR_TG_TOKEN")
